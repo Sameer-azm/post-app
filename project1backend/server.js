58ee86000458498bfb4d87f8d51780d1a5d073fa
sameerazam <sameerazamned@gmail.com>
2026-09-17 02:55:06 +0500

    update

--- project1backend/server.js
+++ project1backend/server.js
@@ -7,6 +7,9 @@ const main=require('./config/db.js')
 main()
 const port=3000
 
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
 })
